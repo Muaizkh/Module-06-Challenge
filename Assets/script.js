43f90f39c8f2363dce8418af66770d71c1c20a77
Muaizkh <khanmu@my.yorku.ca>
2022-11-21 22:10:34 -0500

    changes made to try and fix entire files

--- Assets/script.js
+++ Assets/script.js
@@ -1,83 +1,153 @@
-var apiKey = "7fac45009c803a75ab838aacfa0a79b6";
-var searchButton = $("#search-button");
-// search button to get it to work
-var searchHistoryListEl = $("#search-history");
-// serach history list
-// to keep track of cities searched
 
-function getWeather(lat, lon, type, units, appid) {
-    var apiKey = "7fac45009c803a75ab838aacfa0a79b6";
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={7fac45009c803a75ab838aacfa0a79b6}`;
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            // varaibles that will be used in both of the API fetches as it is neccesarry to get the information that is required and will help better explain the weather 
-            var humidity;
-            var windSpeed 
-            var weather;
+let currentDate = $("#currentDayCity");
+let currentImage = $("#currentDayImg");
+let currentTemp = $("#currenDayTemp");
+let currentWind = $("#currentDayWind");
+let currentHumidity = $("#currentDayHumid");
 
-            // I know need to process the data and make sure to run an if statment to allow for the forecast to actually be read by the HTML and for the api call to work
-            if (type === "forecast") { // API end point for 120 hours of weather split into 3 hour segments
-                var weekdayForecastEl = $("#weekday-forcast"); // Container element
-                weekDayForecastEl.html(""); // Reset the forecast container
-                var dailyWeather = [];
-                var currentDay = [];
-            //    data being spereated into days as it is needed to show the forecast for the week
-                data.list.forEach((e) => { 
-                    if (currentDay.length === 0) {
-                        currentDay.push(e);
-                    } else {
-                       
-                        if (currentDay[currentDay.length - 1].dt_txt.split(" ")[0] !== e.dt_txt.split(" ")[0]) {
-                            dailyWeather.push(currentDay); 
-                            currentDay = [e]; 
-                        } else { 
-                            currentDay.push(e);
-                        }
-                    }
-                });
-                if (currentDay.length !== 0) dailyWeather.push(currentDay); 
-                if (dailyWeather.length > 5) dailyWeather.shift(); 
+let cityInput = $("#city_input");
+let searchButton = $("#submit_button");
+let searchForm = $("#search_form");
 
-                dailyWeather.forEach((e) => {
-                   
+let searchHistory = $("#search_history");
+let forecastList = $("#forecast");
+let apiKey = "5d98486ac6ae2251efcbfca76d98cffd";
+let city = "Toronto";
+let historyCache = [];
+let weatherFutureURL = "https://api.openweathermap.org/data/2.5/forecast?units=metric&";
+let weatherCurrentURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+let locationURL = "https://api.openweathermap.org/geo/1.0/direct?";
 
-                    weatherDate = new Date(e[0].dt * 1000).toJSON().split("T")[0]; 
-                    if (e[5]) { 
-                        weatherIcon = e[5].weather[0].icon;
 
-                        weather = e[5].weather[0].description;
-                        windSpeed = e[5].wind.speed;
-                        humidity = e[5].main.humidity;
-                    } else { // Otherwise, use the icon for midnight
-                        weatherIcon = e[0].weather[0].icon;
+function parseFutureWeather(data) {
+    let daysToDisplay = [];
+    data.forEach(element => {
+        let time = moment(element.dt*1000).format("H")
+        if (time == "11") {
+            daysToDisplay.push(element);
+        }
+    })
 
-                        weather = e[0].weather[0].description;
-                        windSpeed = e[0].wind.speed;
-                        humidity = e[0].main.humidity;
-                    }
-                    var temperatureRangeData = getMaxMinTemperature(e); 
-                    temperatureRange = `Low ${temperatureRangeData.low}°C / High ${temperatureRangeData.high}°C`;
+    let forecastChildren = forecastList.children();
+    
+    
+    for (const index in daysToDisplay) {
+        let cardChildren = $(forecastChildren[index]).children();
+        let dayData = daysToDisplay[index];
 
-                });
-
-            } else if (type === "weather") {
-                var todaysForecastEl = $("#todays-forecast"); 
-                todaysForecastEl.html(""); 
-                var currentTemp = Math.round(data.main.temp);
-                var cityName = data.name;
-                weatherDate = new Date(data.dt * 1000).toJSON().split("T")[0]; 
-                weather = data.weather[0].description;
-                weatherIcon = data.weather[0].icon;
-                windSpeed = data.wind.speed;
-                humidity = data.main.humidity;
-                temperatureRange = `Low ${Math.round(data.main.temp_min)}°C / High ${Math.round(data.main.temp_max)}°C`;
-
-        
-               
-            }
-        });
+        // Date
+        cardChildren[0].textContent = moment(dayData.dt*1000).format("M/D/YYYY");
+        // Image
+        $(cardChildren[1]).children()[0].src = imageURL+dayData.weather[0].icon+"@2x.png";
+        // temp
+        cardChildren[2].textContent = "Temp: "+dayData.main.temp+"  \u00B0C";
+        // wind
+        cardChildren[3].textContent = "Wind: "+dayData.wind.speed+"  Km/h";
+        // humid
+        cardChildren[4].textContent = "Humidity: "+dayData.main.humidity+" %";
+    }
 }
 
+// This parses the data for weather for the current day
+function parseCurrentWeather(data) {
+    currentDate.text(data.name+" ("+moment().format("M/D/YYYY")+")");
+    currentImage.attr("src", imageURL+data.weather[0].icon+"@2x.png");
+    currentTemp.text("Temp: "+data.main.temp+" \u00B0C");
+    currentWind.text("Wind: "+data.wind.speed+" Km/h");
+    currentHumidity.text("Humidity: "+data.main.humidity+" %");
+}
+
+// This gets the weather information from the latitude and longitude
+function getWeatherInfo(lat, lon) {
+    let weatherCurrent = weatherCurrentURL+"lat="+lat+"&lon="+lon+"&appid="+apiKey;
+    fetch(weatherCurrent)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        parseCurrentWeather(data);
+    })
+
+    let weather = weatherFutureURL+"lat="+lat+"&lon="+lon+"&appid="+apiKey;
+    fetch(weather)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        parseFutureWeather(data.list);
+    })
+}
+
+
+// This adds search city to history
+function findCity(searchCity, addToHistory) {
+    if (searchCity === null || searchCity === '') {
+        return;
+    }
+
+    if (addToHistory) {
+        addHistory(searchCity);
+    }
+
+    let location = locationURL+"q="+searchCity+"&limit=1&appid="+apiKey;
+    console.log(location);
+    fetch(location)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        getWeatherInfo(data[0].lat, data[0].lon);
+    })
+}
+
+// clears and renders search history
+function renderHistory() {
+    searchHistory.empty();
+    console.log(historyCache)
+    historyCache.forEach(element => {
+        let button = $('<button class="nav_button">');
+        
+        button.text(element);
+
+        searchHistory.append(button);
+    });
+}
+
+// loads history from localstorage
+function loadHistory() {
+    let history = JSON.parse(localStorage.getItem("WeatherHistory"));
+    if (history !== null ) {
+        historyCache = history;
+    }
+
+    renderHistory();
+}
+
+function addHistory(name) {
+    for (const index in historyCache) {
+        if (historyCache[index] === name) {
+            historyCache.splice(index, 1);
+        }
+    }
+
+    historyCache.unshift(name);
+
+    localStorage.setItem("WeatherHistory", JSON.stringify(historyCache));
+
+    renderHistory();
+}
+
+findCity(city);
+loadHistory();
+
+
+searchButton.on("click", function(event){
+    event.preventDefault();
+
+    let text = cityInput.val();
+    findCity(text, true);
+})
+
+searchHistory.on("click", ".nav_button", function(event) {
+    findCity(event.target.textContent, true);
+})
